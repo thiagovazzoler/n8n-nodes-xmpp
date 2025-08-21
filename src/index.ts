44a19e63e46f5afbcd55665760efd7114def8433
Supervisão de TI - Thiago Vazzoler <thiagovl@unimedsulcapixaba.coop.br>
2025-08-21 16:54:40 -0300

    Ajustes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { XmppTrigger } from './nodes/Xmpp/XmppTrigger.node';
-import { XmppSendMessage } from './nodes/Xmpp/XmppSendMessage.node';
 import { XmppSendFile } from './nodes/Xmpp/XmppSendFile.node';
+import { XmppSendMessage } from './nodes/Xmpp/XmppSendMessage.node';
 
-class XmppModule {
-  static nodes = [
-    XmppTrigger,
-    XmppSendMessage,
-    XmppSendFile,
-  ];
-}
+export const nodes = [
+  XmppTrigger,
+  XmppSendFile,
+  XmppSendMessage,
+];
 
-export default {
-  nodes: XmppModule.nodes,
-};
+export const credentials = [];
